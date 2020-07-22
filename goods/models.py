277b0dea2ef215a35f73e80f532a58895809335e
chenxi <chenxi@git.com>
2020-07-22 09:04:40 +0800

    商品新增页面侧边栏展示

--- goods/models.py
+++ goods/models.py
@@ -75,31 +75,6 @@ class TGoodsType(models.Model):
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-from django.db import models
 
 
-class TGoods(models.Model):
-    title = models.CharField(max_length=100, blank=True, null=True)
-    simple_title = models.CharField(max_length=50, blank=True, null=True)
-    num = models.IntegerField(blank=True, null=True)
-    place = models.CharField(max_length=100, blank=True, null=True)
-    material = models.CharField(max_length=50, blank=True, null=True)
-    brand = models.CharField(max_length=50, blank=True, null=True)
-    weight = models.IntegerField()
-    unit = models.CharField(max_length=4)
-    new_price = models.CharField(max_length=100, blank=True, null=True)
-    old_price = models.CharField(max_length=100, blank=True, null=True)
-    keyword = models.CharField(max_length=100, blank=True, null=True)
-    comment = models.CharField(max_length=200, blank=True, null=True)
-    detail = models.TextField(blank=True, null=True)
-    is_comment = models.IntegerField(blank=True, null=True)
-    create_time = models.DateTimeField(blank=True, null=True)
-    delete_time = models.DateTimeField(blank=True, null=True)
-    update_time = models.DateTimeField(blank=True, null=True)
-    status = models.CharField(max_length=50, blank=True, null=True)
-    check_status = models.CharField(max_length=50, blank=True, null=True)
-    type = models.ForeignKey('TGoodsType', models.DO_NOTHING)
 
-    class Meta:
-        managed = False
-        db_table = 't_goods'
